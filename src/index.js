dca699b9a56092ed0d8c2df637e5171f25ef3f6f
Walid Maarad <walsam@hotmail.com>
2020-09-18 12:08:15 +0100

    adding routes and components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import createHistory from 'history/createBrowserHistory';
+import {Provider} from "react-redux";
+import {Switch,Route} from "react-router";
+import {ConnectedRouter} from "react-router-redux";
+import App from "./components/App";
+import LoginForm from "./components/LoginForm";
+
+const store = createStore(
+    state => state
+);
+const history = createHistory();
 
 ReactDOM.render((
-    <div>Hello world!</div>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route path="/login" component={LoginForm}/>
+                <Route path="/" component={App}/>
+            </Switch>
+        </ConnectedRouter>
+    </Provider>
 ), document.getElementById('root'));
