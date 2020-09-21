eb7ce947bd66a733071cf1e88f911989bd792808
Walid Maarad <walsam@hotmail.com>
2020-09-21 19:29:21 +0100

    getting the data of blogpost

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,13 @@
 import {requests} from "../agent";
 
-export const BLOG_POST_LIST_REQUEST = 'BLOG_POST_REQUEST';
-export const BLOG_POST_LIST_RECEIVED = 'BLOG_POST_LIST_RECEIVED';
-export const BLOG_POST_LIST_ERROR = 'BLOG_POST_LIST_ERROR';
-export const BLOG_POST_LIST_ADD = 'BLOG_POST_LIST_ADD';
+import {
+    BLOG_POST_ERROR,
+    BLOG_POST_LIST_ADD,
+    BLOG_POST_LIST_ERROR,
+    BLOG_POST_LIST_RECEIVED,
+    BLOG_POST_LIST_REQUEST, BLOG_POST_RECEIVED,
+    BLOG_POST_REQUEST
+} from "./constants";
 
 export const blogPostListRequest = () => ({
     type: BLOG_POST_LIST_REQUEST,
@@ -28,6 +32,28 @@ export const blogPostListFetch = () => {
     }
 };
 
+export const blogPostRequest = () => ({
+    type: BLOG_POST_REQUEST,
+});
+
+export const blogPostError = (error) => ({
+    type: BLOG_POST_ERROR,
+    error
+});
+
+export const blogPostReceived = (data) => ({
+    type: BLOG_POST_RECEIVED,
+    data
+});
+
+export const blogPostFetch = (id) => {
+    return (dispatch) => {
+        dispatch(blogPostRequest());
+        return requests.get(`/blog_posts/${id}`)
+            .then(response => dispatch(blogPostReceived(response)))
+            .catch(error => dispatch(blogPostError(error)));
+    }
+};
 
 export const blogPostAdd = () => ({
     type: BLOG_POST_LIST_ADD,
