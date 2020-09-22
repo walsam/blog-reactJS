8fafe057cb639c54ac2a5ad4d8187021e3a1b7c6
Walid Maarad <walsam@hotmail.com>
2020-09-22 12:29:34 +0100

    adding UserLoginSuccess action + auth reducer

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -8,7 +8,13 @@ import {
     BLOG_POST_LIST_REQUEST,
     BLOG_POST_RECEIVED,
     BLOG_POST_REQUEST,
-    BLOG_POST_UNLOAD, COMMENT_LIST_ERROR, COMMENT_LIST_RECEIVED, COMMENT_LIST_REQUEST, COMMENT_LIST_UNLOAD
+    BLOG_POST_UNLOAD,
+    COMMENT_LIST_ERROR,
+    COMMENT_LIST_RECEIVED,
+    COMMENT_LIST_REQUEST,
+    COMMENT_LIST_UNLOAD,
+    USER_LOGIN_SUCCESS
+
 } from "./constants";
 
 export const blogPostListRequest = () => ({
@@ -86,6 +92,24 @@ export const commentListFetch = (id) => {
         return requests.get(`/blog_posts/${id}/comments`)
             .then(response => dispatch(commentListReceived(response)))
             .catch(error => dispatch(commentListError(error)));
+    }
+};
+
+export const userLoginSuccess = (token, userId) => {
+    return {
+        type: USER_LOGIN_SUCCESS,
+        token,
+        userId
+    }
+};
+
+export const userLoginAttempt = (username, password) => {
+    return (dispatch) => {
+        return requests.post('/login_check', {username, password}, false).then(
+            response => dispatch(userLoginSuccess(response.token, response.id))
+        ).catch(error => {
+            console.log('Login failed');
+        });
     }
 };
 
