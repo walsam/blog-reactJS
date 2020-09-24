397b0dacbe7a3d57508db8317fdf78853e4d621c
Walid Maarad <walsam@hotmail.com>
2020-09-24 18:18:07 +0100

    comments are working, but need refreshing to show

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -2,15 +2,27 @@ import React from "react";
 import {Field, reduxForm} from "redux-form";
 import {connect} from "react-redux";
 import {renderField} from "../form";
+import {commentAdd} from "../actions/actions";
+
+const mapDispatchToProps = {
+    commentAdd
+};
 
 class CommentForm extends React.Component {
+    onSubmit(values) {
+        const {commentAdd, blogPostId} = this.props;
+        return commentAdd(values.content, blogPostId);
+    }
     render() {
+        const {handleSubmit, submitting} = this.props;
+
         return (
             <div className="card mb-3 mt-3 shadow-sm">
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                         <Field name="content" label="Type your comment:" type="textarea" component={renderField}/>
-                        <button type="submit" className="btn btn-primary btn-big btn-block">
+                        <button type="submit" className="btn btn-primary btn-big btn-block"
+                                disabled={submitting}>
                             Add Comment
                         </button>
                     </form>
@@ -22,4 +34,4 @@ class CommentForm extends React.Component {
 
 export default reduxForm({
     form: 'CommentForm'
-})(connect(null, null)(CommentForm))
+})(connect(null, mapDispatchToProps)(CommentForm))
